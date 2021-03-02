9c43ab631e5f10c83176f9c8efdb2d1e093a52d7
mathmatowicki <matowicki@gmail.com>
2021-03-02 15:56:43 +0100

    Implement fizzbuzz alghoritm

--- Program.cs
+++ Program.cs
@@ -6,7 +6,43 @@ namespace fizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int number = PromptNumber();
+
+            FizzBuzz(number);
+        }
+
+        private static int PromptNumber()
+        {
+            int number = 0;
+            Console.WriteLine("Please type the number");
+            string line = Console.ReadLine();
+            while (!int.TryParse(line, out number))
+            {
+                Console.WriteLine("Error:" + line + " is not an integer\n" + "Try put integer instead!");
+                line = Console.ReadLine();
+
+            }
+            return number;
+        }
+        private static void FizzBuzz(int number)
+        {
+            if (number % 3 == 0)
+            {
+                Console.Write("fizz");
+                if (number % 5 == 0)
+                {
+                    Console.Write("buzz");
+                }
+            }
+            else if (number % 5 == 0)
+            {
+                Console.Write("buzz");
+            }
+            else
+            {
+                Console.WriteLine(number);
+            }
         }
     }
 }
